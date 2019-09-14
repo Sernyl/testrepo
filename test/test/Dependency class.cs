f87b6f51c537e748c5a2032a147eefd096b78f08
Unknown <sernyl@mail.ru>
2019-09-14 19:07:16 +0500

    1

---
+++ test/test/Dependency class.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test
+{
+    public interface IDependencyClass
+    {
+        int GetSomeName();
+    }
+
+    public class DependencyClass : IDependencyClass
+    {
+        public int GetSomeName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/test/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test
+{
+    public interface IMyClass
+    {
+        void Work();
+    }
+
+    public class MyClass : IMyClass
+    {
+        public void Work()
+        {
+            var dep = new DependencyClass();
+            Console.WriteLine(dep.GetSomeName());
+        }
+    }
+}
